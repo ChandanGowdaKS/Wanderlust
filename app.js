7fd62cf082f69a9f7b2d62bfea1003ede953f26e
Chandan Gowda K S <chanduchess122@gmail.com>
2026-01-23 22:19:03 +0530

    Initial

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const listing = require("./modules/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");   
+const ejsMate = require("ejs-mate");
+
 
 
 main().then(() => {
@@ -38,6 +40,8 @@ app.get("/",(req, res)=> {
 })
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 //index Route
 
